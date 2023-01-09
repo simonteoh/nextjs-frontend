08b6bc2015d981922982362d86bafb712062c622
simonteoh1996 <simonteoh1996@gmail.com>
2023-01-10 00:38:47 +0800

    #1 first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Link from "next/link";
+import Navbar from '../components/Navbar';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+
+    </div>
+    
+  )
 }
